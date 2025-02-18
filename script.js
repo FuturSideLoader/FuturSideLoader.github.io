bc28adcf9ce4d60b6481ba5f68c245a4f23c08e2
AlexisGueydon <131589270+AlexisGueydon@users.noreply.github.com>
2025-02-18 03:18:22 +0100

    Download games and mods

---
+++ script.js
@@ -0,0 +1,8 @@
+function downloadGame(gameId) {
+    alert('Downloading ' + gameId);
+    // Ici, tu pourras ajouter la logique de téléchargement après vérification de l'abonnement
+}
+
+function login() {
+    // Ici, on ajoutera la logique de connexion avec Firebase
+}
